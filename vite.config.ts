ea2cec67931871fa8938368bbbf126078ba3848d
weberjavi <weberjavi@gmail.com>
2025-09-11 20:06:42 +0200

    update path

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/admins-map/' : '/',
+  base: command === "build" ? "/admins-map" : "/",
   plugins: [react()],
   server: {
     headers: {
       // Enable proper headers for PMTiles files
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Range',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Range",
     },
   },
   preview: {
     headers: {
       // Enable proper headers for PMTiles files in preview mode
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Range',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Range",
     },
   },
-}))
+}));
