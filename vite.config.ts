1ada7a548e1f9e1c770418b4cae1542aefaf2ef8
weberjavi <weberjavi@gmail.com>
2025-09-11 20:12:55 +0200

    /

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/admins-map" : "/",
+  base: command === "build" ? "/admins-map/" : "/",
   plugins: [react()],
   server: {
     headers: {
